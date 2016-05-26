6fe3d78253c0aeafe72d8b1204143294a3a473c1
Richard Hattersley <rhattersley@gmail.com>
2016-05-26 14:22:34 +0100

    Simple JSON API

---
+++ src/main/java/hello/Coverage.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class Coverage {
+
+    public String thing;
+    public int number;
+
+    public Coverage(String thing, int number) {
+        this.thing = thing;
+        this.number = number;
+    }
+}
--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -5,13 +5,13 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class SampleController {
     @RequestMapping("/")
     @ResponseBody
-    String home() {
-        return "Hello World!";
+    public Coverage home() {
+        return new Coverage("This", 12);
     }
 
     public static void main(String[] args) throws Exception {
